b361f2a6a02ba35c65d064d9e8714aed1a6699e3
Yuri <yurimkrtumyan02@gmail.com>
2023-04-01 20:59:08 +0400

    2nd cmt

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class TestClass {
@@ -42,9 +41,53 @@ public class TestClass {
 
         WebElement logBTN = driver.findElement(By.id("loginbtn"));
         logBTN.click();
+
+
+        String pageTitle = driver.getTitle();
+        String expectedTitle = "E-learning @ AUA: Log in to the site";
+
         String errorMessage = driver.findElement(By.id("loginerrormessage")).getText();
+
+
+        assertEquals(expectedTitle, pageTitle);
         assertTrue(errorMessage.contains("Invalid login, please try again"));
 
+
+    }
+
+    // I was confused to test login with correct password and username so I checked with my data it worked
+    // if you wanna check below test just put your valid username and password and UNCOMMENT @TEST
+
+    //@Test
+    public void testLogin2() {
+        String yourValidPassword = "";
+        String yourValidUserName = "";
+
+        driver.get("https://elearning.aua.am/");
+
+        WebElement spanElement = driver.findElement(By.className("login"));
+        WebElement aElement = spanElement.findElement(By.xpath(".//a"));
+        aElement.click();
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys(yourValidUserName);
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys(yourValidPassword);
+
+        WebElement logBTN = driver.findElement(By.id("loginbtn"));
+        logBTN.click();
+
+
+        String pageTitle = driver.getTitle();
+        String expectedTitle = "Dashboard";
+
+
+        String currentUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://elearning.aua.am/my/";
+
+        assertEquals(expectedTitle, pageTitle);
+        assertTrue(currentUrl.contains(expectedUrl));
 
     }
 
